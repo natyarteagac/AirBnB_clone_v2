5444b6052673b767a08f3d9174d803a8ad0cbbc6
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-09 17:03:31 -0500

    Updating files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,4 +5,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db .* TO 'hbnb_dev'@'localhost' IDENTIFIED BY '
 GRANT SELECT ON performance_schema .* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
-
