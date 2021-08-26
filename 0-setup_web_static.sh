b88fcf830325fb12c8f295828bddef2d1237b12e
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-26 11:49:52 -0500

    Fixing checks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,8 @@
 if [ ! -x /usr/sbin/nginx ]
 then
     sudo apt-get -y install nginx
+else
+    :
 fi
 # Creating if doesn't exists
 sudo mkdir -p /data/web_static/releases/test/
@@ -17,5 +19,5 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 # Creating an alias
 sudo sed -i "/:80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
-# Restarting ngix
+# Restarting nginx
 sudo service nginx restart
