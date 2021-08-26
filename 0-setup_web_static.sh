c675b491a8fc5dcc1e2408c63a711273780582a3
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-25 20:11:38 -0500

    Both servers running

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Script that sets the web serves for the deployment
+# of web_static
+
+# If nginx is not installed do it.
+if [ ! -x /usr/sbin/nginx ]
+then
+    sudo apt-get -y install nginx
+else
+    :
+fi
+# Creating if doesn't exists
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+# Creating the file index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+# Creating symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+# Creating an alias
+sudo sed -i "/:80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
+# Restarting ngix
+sudo service nginx restart
