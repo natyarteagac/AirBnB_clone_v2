6d9e69acd78b4e41ff843d544fdcb93204c6d124
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-26 15:59:24 -0500

    Fixing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # Script that sets the web serves for the deployment
 
-# Install Nginx if it not already installed
+# Installing Nginx if it not already installed
 if ! dpkg -s nginx  > /dev/null
 then
 	apt-get -y update
 	apt-get -y upgrade
 	apt-get -y install nginx
 fi
-
 # Writing the string on the index.nginx-debian.html
 echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html
 # Redirecting
@@ -17,12 +16,13 @@ sudo sed -i '/server_name _;/ a \\trewrite ^/redirect_me https://www.youtube.com
 echo "Ceci n'est pas une page" | sudo tee /usr/share/nginx/html/custom_404.html
 # Appending information in default file
 sudo sed -i "20i \\\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t root /usr/share/nginx/html;\n\tinternal;\n\t}" /etc/nginx/sites-available/default
-# Creating the file index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sed -i "/http {/ a \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf
+
 # Creating if doesn't exists
 sudo mkdir -p /data/web_static/releases/test/
-# Creating if doesn't exists
 sudo mkdir -p /data/web_static/shared/
+# Creating the file index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 # Creating symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Giving ownership of data to ubuntu and group
