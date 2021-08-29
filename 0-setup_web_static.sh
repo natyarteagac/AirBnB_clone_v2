171ca0f5ea4d36ddfb8fa0ae395f6b2b836d7413
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-29 11:29:07 -0500

    Changed code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,13 @@ fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 # Creating the file index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+touch /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 # Creating symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Giving ownership of data to ubuntu and group
 sudo chown -R ubuntu:ubuntu /data/
+sudo chmod -R 755 /data/
 # Creating an alias
 sudo sed -i '/:80 default_server;/ a \\\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}/' /etc/nginx/sites-available/default
 # Restarting nginx
