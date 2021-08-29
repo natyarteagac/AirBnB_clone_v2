8b1adae66b645924132932aff62f9adaaabf8fc4
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-29 13:49:53 -0500

    Done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,18 +26,18 @@ def do_deploy(archive_path):
     if path.isfile(archive_path) is False:
         return False
     file_token_one = archive_path.split("/")[-1]
-    file_token_two = archive_token.split(".")[0]
+    file_token_two = file_token_one.split(".")[0]
     path_tokenized = ("/data/web_static/releases/" +
-                      archive_token.split(".")[0])
+                      file_token_two.split(".")[0])
     try:
         put(archive_path, '/tmp/')
         run("sudo mkdir -p {}".format(path_tokenized))
         run("sudo tar -xzf /tmp/{} -C {}".format(
-            file_token_two, path_tokenized))
-        run("sudo rm /tmp/{}".format(file_token_two))
+            file_token_one, path_tokenized))
+        run("sudo rm /tmp/{}".format(file_token_one))
         run("sudo mv {}/web_static/* {}/".format(
-            file_tokenized, path_tokenized))
-        run("sudo rm -rf {}/web_static/".format(
+            path_tokenized, path_tokenized))
+        run("sudo rm -rf {}/web_static".format(
             path_tokenized))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(path_tokenized))
