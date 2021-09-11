fbb4d2d00768a5c254150fbaf95cadb53348bbaf
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-11 15:43:28 -0500

    Fixed code

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,24 @@
-from flask import Flask, escape
+#!/usr/bin/python3
 """ Creating the first Flask Application
 """
+from flask import Flask, escape
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_route():
     """ Returning the string Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Returning the string HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def show_the_string(text):
     """Returning the string sent by shell"""
     return 'C %s' % escape(text).replace("_", " ")
