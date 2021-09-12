db95f0be892ed92be693cee97c900af6d3399f49
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-12 12:45:58 -0500

    Checking

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" List of cities """
+
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+elements = storage.all()
+
+
+@app.route('/states_list')
+def states_list(elements):
+    """ Returns HTML with query """
+    return render_template('7-states_list.html', elements=elements)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close the session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
